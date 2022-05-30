2985e5041f9457268eda5ed159218058989573a0
Dezhi Huang <cshdzxjtu@163.com>
2022-05-30 23:51:46 +0800

    add

---
+++ src/main/java/com/hdz/collectionsutil/App.java
@@ -0,0 +1,13 @@
+package com.hdz.collectionsutil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
